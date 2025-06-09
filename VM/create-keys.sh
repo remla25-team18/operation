9549ec1c0463ae967b73cb99346b492b50b2cf78
Yahor Dziomin <y.y.dziomin@student.tudelft.nl>
2025-06-09 10:14:01 +0200

    create-keys is idempotent now. running multiple time will not create new keys if they already exist

--- VM/create-keys.sh
+++ VM/create-keys.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 # This script generates SSH keys for Ansible provisioning
-mkdir ./ssh
-ssh-keygen -t rsa -b 4096 -f ./ssh/ansible-provision-key -C "remla25-18" -N ""
 
+# Create ssh directory only if it doesn't exist
+mkdir -p ./ssh
 chmod 700 ./ssh
-chmod 600 ./ssh/ansible-provision-key
+
+KEY_PATH=./ssh/ansible-provision-key
+
+# Generate keys only if they don't exist
+if [ ! -f "$KEY_PATH" ]; then
+  ssh-keygen -t rsa -b 4096 -f "$KEY_PATH" -C "remla25-18" -N ""
+  chmod 600 "$KEY_PATH"
+fi
+
+# Ensure correct permissions even if keys are already there
+chmod 600 "$KEY_PATH"
